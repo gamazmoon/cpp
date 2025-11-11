3b9b28d93702942a13cb299070ba65290d16606b
gamazmoon <gamazmooncom@gmail.com>
2025-11-11 19:09:04 +0400

    escape-characters

---
+++ 01-output/03-escape-characters/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n 1----------- \n";
+    // 1. \n (خط جدید - Newline)
+    cout << "test1\ntest2";
+
+    cout << "\n 2----------- \n";
+    cout << "test1\ttest2";
+
+    // 3. \" (نقل قول دوبل - Double Quote)
+    cout << "\n 3----------- \n";
+    cout << "test1\"test2";
+
+    // 4. \\ (بک‌اسلش - Backslash)
+    cout << "\n 4----------- \n";
+    cout << "test1\\test2";
+
+    return 0;
+}
