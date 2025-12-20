7198bd9477d09adf906f94e6d12f01b5a50a14fd
gamazmoon <gamazmooncom@gmail.com>
2025-12-20 10:05:49 +0400

    Variables

---
+++ 05-variables/01-defination/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    a = 5;
+    b = 6;
+
+    c = a + b;
+
+    cout << c;
+    cout<<"\n";
+    cout<<"c";
+
+    return 0;
+}
