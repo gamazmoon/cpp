9c0b88af66541899a06260224de5505a0da73fa9
gamazmoon <gamazmooncom@gmail.com>
2025-12-21 01:33:53 +0400

    Data types

---
+++ 05-variables/02-data-types/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    int num1;
+    int num2;
+    double num3;
+    double num4;
+    bool boolVal;
+    char myChar;
+    string message;
+
+    // Assign values to variables
+    num1 = 1;
+    num2 = 2;
+    num3 = 3.54;
+    num4 = 4.12;
+    boolVal = true; // boolVal = false;
+    myChar = '@';
+    message = "Hello World!";
+
+    // Show the values of the variables
+    cout << "num1 = " << num1 << '\n';
+    cout << "num2 = " << num2 << '\n';
+    cout << "num3 = " << num3 << '\n';
+    cout << "num4 = " << num4 << '\n';
+    cout << "boolVal = " << boolVal << '\n';
+    cout << "myChar = " << myChar << '\n';
+    cout << "message = " << message << '\n';
+}
