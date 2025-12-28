ab76c2d1bcb62845b6d14360a3afe7484748c3d0
gamazmoon <gamazmooncom@gmail.com>
2025-12-28 14:56:36 +0400

    input

--- 07-input/main.cpp
+++ 07-input/main.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 /*
 یک ماشین حساب ساده
+دریافت اعداد از کاربر
 */
 
 int main()
 {
-    int a = 20, b = 5, z;
+    int a, b, z;
+
+    cout<<"Enter a:";
+    cin >> a;
+
+    cout<<"Enter b:";
+    cin >> b;
 
     z = a * b;
     cout << "a*b is:" << z << endl;
