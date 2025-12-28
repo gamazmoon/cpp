5f2bd8928d5afe4c940cc17c3a3810e711fe0e6b
gamazmoon <gamazmooncom@gmail.com>
2025-12-28 18:46:27 +0400

    simple if-else

---
+++ 08-if/02-simple-if-else/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 5;
+    // Test the condition
+    if (number < 10)
+    {
+        cout << "The number is less than 10." << endl;
+    }
+    else
+    {
+        cout << "The number is either greater than or equal to 10." << endl;
+    }
+    
+    // Modify value of number
+    number = 15;
+    // Repeat the test to yield a different result
+    if (number < 10)
+    {
+        cout << "The number is less than 10." << endl;
+    }
+    else
+    {
+        cout << "The number is either greater than or equal to 10." << endl;
+    }
+}
