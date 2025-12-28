19a67796fafe54b40f66758161eb291d69fd3628
gamazmoon <gamazmooncom@gmail.com>
2025-12-28 11:47:14 +0400

    calculator

---
+++ 05-variables/07-calculator/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+جا به جایی مقدار دو متغیر
+*/
+
+int main()
+{
+    int a = 20, b = 5, z;
+
+    z = a * b;
+    cout << "a*b is:" << z << endl;
+
+    z = a / b;
+    cout << "a/b is:" << z << endl;
+
+    z = a + b;
+    cout << "a+b is:" << z << endl;
+
+    z = a - b;
+    cout << "a-b is:" << z << endl;
+
+    return 0;
+}
