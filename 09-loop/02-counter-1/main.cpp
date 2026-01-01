c98f90dec431a2a65bd696b13974eb0780fe4c4c
gamazmoon <gamazmooncom@gmail.com>
2026-01-01 23:00:57 +0400

    counter start from 1

---
+++ 09-loop/02-counter-1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter = 1;
+    while (counter <= 5)
+    {
+        cout << "Hello World!" << endl;
+
+        counter++;
+    }
+
+    cout<<counter;
+    return 0;
+}
