3b2d5a1c4e7d8aab6aea4ae9fbf924231c18c355
gamazmoon <gamazmooncom@gmail.com>
2026-01-01 23:06:16 +0400

    counter 2 example

---
+++ 09-loop/02-counter/02-counter-2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter = 11;
+    while (counter <= 15)
+    {
+        cout << "Hello World!" << endl;
+
+        counter++;
+    }
+
+    cout<<counter;
+    return 0;
+}
