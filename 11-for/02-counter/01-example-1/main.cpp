a2771296eae98b2bb1cb1ecac2f0eb9f726ca637
gamazmoon <gamazmooncom@gmail.com>
2026-01-04 02:58:59 +0400

    for

---
+++ 11-for/02-counter/01-example-1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter;
+    for (counter = 1; counter <= 5; counter++)
+    {
+        cout << "Hello World!" << endl;
+    }
+
+    return 0;
+}
