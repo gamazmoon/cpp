043ecd24fe70ebb9fda58604a1b1f2f4b1a0c961
gamazmoon <gamazmooncom@gmail.com>
2026-01-03 15:26:30 +0400

    pattern-rectangle-stars: get values from user

--- 09-loop/07-nested-loops/02-pattern-rectangle-stars/main.cpp
+++ 09-loop/07-nested-loops/02-pattern-rectangle-stars/main.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main()
 {
-    int num_of_rows = 3;
-    int num_of_cols = 5;
+    int num_of_rows;
+    int num_of_cols;
+
+    cout << "Enter number of rows:";
+    cin >> num_of_rows;
+
+    cout << "Enter number of cols:";
+    cin >> num_of_cols;
 
     int i = 1;
     while (i <= num_of_rows)
