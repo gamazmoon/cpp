de1e56dc4a2f814dbad6a053ef70eb1509ac26fc
gamazmoon <gamazmooncom@gmail.com>
2026-01-04 14:39:45 +0400

    rename loop to while

---
+++ 11-loops-for/02-even-numbers/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 4; i++)
+    {
+    }
+
+    for (i = 1; i <= 8; i++)
+    {
+    }
+
+    for (i = 2; i <= 8; i += 2)
+    {
+    }
+
+    return 0;
+}
