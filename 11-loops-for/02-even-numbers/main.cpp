cdbc6c132233f0d3cb10d447623e3500f28488f3
gamazmoon <gamazmooncom@gmail.com>
2026-01-04 16:27:13 +0400

    even numbers with for

--- 11-loops-for/02-even-numbers/main.cpp
+++ 11-loops-for/02-even-numbers/main.cpp
@@ -7,14 +7,24 @@ int main()
 
     for (i = 1; i <= 4; i++)
     {
+        cout << i * 2 << "\t";
     }
+    cout << "\n---------------\n";
 
     for (i = 1; i <= 8; i++)
     {
+        if (i % 2 == 1) // عدد فرد است
+        {
+            continue;
+        }
+        cout << i << "\t";
     }
+
+    cout << "\n---------------\n";
 
     for (i = 2; i <= 8; i += 2)
     {
+        cout << i << "\t";
     }
 
     return 0;
