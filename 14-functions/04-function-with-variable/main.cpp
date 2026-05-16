9cb97f292c6cedbd33729a3d0924c884eca9a3b9
gamazmoon <gamazmooncom@gmail.com>
2026-05-16 18:36:48 +0330

    introduction to functions

---
+++ 14-functions/04-function-with-variable/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int f()
+{
+    int a = 10;
+    return a;
+}
+
+int main()
+{
+    int a = f();
+    cout << a;
+
+    return 0;
+}
---
+++ 14-functions/06-double-return-type/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+double f()
+{
+    return 10.5;
+}
+
+int main()
+{
+    double a = f();
+    cout << a;
+
+    return 0;
+}
